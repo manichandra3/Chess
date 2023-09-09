2c7d5674fab279b699ded741d8784c2bb38776dd
manichandra3 <127130821+manichandra3@users.noreply.github.com>
2023-09-09 23:34:31 +0530

    Added Java classes Piece and Tile
    The class files are incomplete and need some more methods to be more functional. The individual piece sub-classes also need to be worked on.

---
+++ src/Tile.java
@@ -0,0 +1,40 @@
+public abstract class Tile {
+    int tileCoordinate;
+
+    Tile(int tileCoordinate) {
+        this.tileCoordinate = tileCoordinate;
+    }
+    public abstract boolean isOccupied();
+    public abstract Piece getPiece();
+    public static final class EmptyTile extends Tile {
+        EmptyTile(int coordinate) {
+            super(coordinate);
+        }
+        @Override
+        public boolean isOccupied() {
+            return false;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return null;
+        }
+    }
+    public static final class OccupiedTile extends Tile {
+        Piece pieceOnTile;
+
+        OccupiedTile(int tileCoordinate) {
+            super(tileCoordinate);
+        }
+
+        @Override
+        public boolean isOccupied() {
+            return true;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return this.pieceOnTile;
+        }
+    }
+}
