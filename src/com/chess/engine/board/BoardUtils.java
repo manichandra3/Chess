3dec42106eadff136f12968361a15cf09b2fa6f9
manichandra3 <manichandra371@gmail.com>
2024-12-08 11:33:13 +0530

    implemented better error handling and row logic

--- src/com/chess/engine/board/BoardUtils.java
+++ src/com/chess/engine/board/BoardUtils.java
@@ -2,32 +2,42 @@ package com.chess.engine.board;
 
 public class BoardUtils {
     public static final boolean[] FIRST_COLUMN = initColumn(0);
-    public static final boolean[] SECOND_COLUMN  = initColumn(1);
+    public static final boolean[] SECOND_COLUMN = initColumn(1);
     public static final boolean[] SEVENTH_COLUMN = initColumn(6);
     public static final boolean[] EIGHTH_COLUMN = initColumn(7);
 
-    public static final boolean[] SEVENTH_ROW = null;
-    public static final boolean[] SECOND_ROW = null;
+    public static final boolean[] FIRST_ROW = initRow(0);
+    public static final boolean[] SECOND_ROW = initRow(1);
+    public static final boolean[] SEVENTH_ROW = initRow(6);
+    public static final boolean[] EIGHTH_ROW = initRow(7);
 
     public static final int NUM_TILES = 64;
     public static final int NUM_TILES_PER_ROW = 8;
 
-
-    private BoardUtils(){
-        throw new RuntimeException("Can not be instantiated.");
+    private BoardUtils() {
+        throw new RuntimeException("Cannot be instantiated.");
     }
+
     private static boolean[] initColumn(int columnNumber) {
         final boolean[] column = new boolean[NUM_TILES];
-        do{
-            column[columnNumber]=true;
+        do {
+            column[columnNumber] = true;
             columnNumber += NUM_TILES_PER_ROW;
-
-        } while(columnNumber < NUM_TILES);
+        } while (columnNumber < NUM_TILES);
         return column;
     }
-    public static boolean isValidTileCoordinate(final int coordinate) {
 
+    private static boolean[] initRow(int rowNumber) {
+        final boolean[] row = new boolean[NUM_TILES];
+        for (int i = 0; i < NUM_TILES_PER_ROW; i++) {
+            row[rowNumber * NUM_TILES_PER_ROW + i] = true;
+        }
+        return row;
+    }
+
+    public static boolean isValidTileCoordinate(final int coordinate) {
         return coordinate >= 0 && coordinate < NUM_TILES;
     }
 
+    // Additional utility methods can be added here
 }
